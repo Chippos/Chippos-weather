f2c279b4a7627053efa8ebaf7a8c2077837b6f14
Usama <jusamaoct@gmail.com>
2022-07-06 18:34:05 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [coords, setCoords] = useState("");
+
+  let url= `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=298dde8a266bb981b53ffae341d0530d`
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      // ctns.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  async function showPosition(position) {
+     url = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=298dde8a266bb981b53ffae341d0530d`
+    ).then(response => {
+      setData(response.data);
+    });
+
+    setCoords(`Latitude: ${position.coords.latitude}  
+    Longitude: ${position.coords.longitude}`);
+  }
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation('')
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          type="text"
+          placeholder="City"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+            {data.sys? <p>{data.sys.country}</p>: null}
+          </div>
+          <div className="temperature">
+            {data.main? <h1>{data.main.temp.toFixed()}°C</h1>:null}
+          </div>
+          <div className="desc">
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="bottom-inner feels">
+          {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+            <p className="bottom-desc">Feels Like</p>
+          </div>
+          <div className="bottom-inner humidity">
+          {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p className="bottom-desc">Humidity</p>
+          </div>
+          <div className="bottom-inner wind">
+          {data.wind ? <p className='bold'>{(data.wind.speed*1.6).toFixed()} Kmh</p> : null}
+            <p className="bottom-desc">Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
